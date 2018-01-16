2bf0d3f2fe30997915f96ccc74e6f550b3ba8cf2
S. Wingchi Wong <stephenwong@intrepid.io>
2018-01-16 00:50:49 -0500

    Add html and event listeners

--- todoapp-angular/src/app/app.component.ts
+++ todoapp-angular/src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent implements OnInit {
   public newTodo: ToDo = new ToDo()
 
   todosList: ToDo[]
+  editTodos: ToDo[] = []
 
   ngOnInit(): void {
     this.todoService.getTodos()
@@ -23,5 +24,55 @@ export class AppComponent implements OnInit {
       this.todosList = todos
       console.log(todos)
     })
+  }
+
+  // key press event listeners
+  submitTodo(event, todo: ToDo) {
+    if (event.keyCode === 13) {
+      this.editTodo(todo)
+    }
+  }
+
+  // button click event listeners
+  createTodo() {
+    this.todoService.createTodo(this.newTodo)
+      .subscribe(res => {
+        this.todosList.push(res.data)
+        this.newTodo = new ToDo()
+      })
+  }
+
+  editTodo(todo: ToDo) {
+    if (!this.todosList.includes(todo)) { return }
+    if (!this.editTodos.includes(todo)) {
+      this.editTodos.push(todo)
+    } else {
+      this.editTodos.splice(this.editTodos.indexOf(todo), 1)
+      this.todoService.editTodo(todo)
+        .subscribe(res => {
+          console.log('Update Successful')
+        }, err => {
+          this.editTodo(todo)
+          console.error('Update Unsucessful')
+        })
+    }
+  }
+
+  finishTodo(todo: ToDo) {
+    todo.status = 'Done'
+    this.todoService.editTodo(todo)
+      .subscribe(res => {
+        console.log('Update Successful')
+      }, err => {
+        this.editTodo(todo)
+        console.error('Update Unsuccessful')
+      })
+  }
+
+  deleteTodo(todo: ToDo) {
+    this.todoService.deleteTodo(todo._id)
+      .subscribe(res => {
+        this.todosList.splice(this.todosList.indexOf(todo), 1)
+      })
   }
 }
--- todoapp-angular/src/app/services/todo.service.ts
+++ todoapp-angular/src/app/services/todo.service.ts
@@ -39,4 +39,9 @@ export class TodoService {
       return res
     })
   }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error)
+    return Promise.reject(error.message || error)
+  }
 }
